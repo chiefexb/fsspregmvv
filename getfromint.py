b125547ff923b2c9fb58eb7a39bf281dc86ddf25
chiefexb <chief_exb@mail.ru>
2013-12-17 09:13:50 +0400

    save last change

--- getfromint.py
+++ getfromint.py
@@ -22,18 +22,28 @@ def main():
  reg=[]
  for i in range(0,len(cfg.file_scheme)):
   field=[]
-  if len(cfg.file_scheme[i])==3:
-   field.append(cfg.file_scheme[i]['field_name'])
-   field.append(cfg.file_scheme[i]['field_type'])
-   field.append(cfg.file_scheme[i]['field_size'])
+  field.append(cfg.file_scheme[i]['field_name'])
+  field.append(cfg.file_scheme[i]['field_type'])
+  field.append(cfg.file_scheme[i]['field_size'])
+  if len(cfg.file_scheme[i])==4:
+   field.append(cfg.file_scheme[i]['field_dec'])
   db.addField(field)
   reg.append(field)
  print db
- print reg[1][0]
+ print reg[1][0],reg
+#Соединяемся с базой ОСП
+ 
+ try:
+  con = fdb.connect (host=cfg.hostname, database=cfg.database, user=cfg.username, password=cfg.password,charset=cfg.database_connection_codepage)
+ except  Exception, e:
+  print("Ошибка при открытии базы данных:\n"+str(e))
+  sys.exit(2)
+ cur = con.cursor() 
  db.close() 
  
  #print const[cfg.reqxml[0]['node']] 
  #print cfg.reqxml[0]['node']
  f.close()
+ con.close()
 if __name__ == "__main__":
     main()
