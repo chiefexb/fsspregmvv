4731cc602ff4f126682834d3284f677b848a61f6
chiefexb <chief_exb@mail.ru>
2013-12-21 20:22:46 +0400

    Добавил  Обработку схемы файла Пока умеет читать root xml

--- getfromint.py
+++ getfromint.py
@@ -38,7 +38,20 @@ def main():
  agent_code=mvv.find('agent_code').text
  dept_code=mvv.find('dept_code').text
  agreement_code=mvv.find('agreement_code').text
-# f.close()
+ #f.close()
+#Определяем тип и путь файла
+ filepar=cfgroot.find('file')
+ fliecodepage=filepar.find('codepage').text
+ output_path=filepar.find('output_path').text
+ filetype=filepar.find('type').text
+ filenum=filepar.find('numeric').text
+ #Определение схемы файла должна быть ветка для типов файлов пока разбираем xml
+ filiescheme=filepar.find('scheme')
+ #создание root
+ print filiescheme.getchildren()[0].tag
+ root=etree.Element(filiescheme.getchildren()[0].tag)
+ #Определение заголовка
+
 #Соединяемся с базой ОСП
  try:
   con = fdb.connect (host=hostname, database=database, user=username, password=password,charset=concodepage)
@@ -46,7 +59,8 @@ def main():
   print("Ошибка при открытии базы данных:\n"+str(e))
   sys.exit(2)
  cur = con.cursor() 
-#Определяем список необработанных
+#Предварительная обработка 
+#Определяем список необработанных пакетов
  packets=getnotprocessed(cur,systemcodepage,'CP1251',mvv_agent_code=agent_code,mvv_agreement_code=agreement_code,mvv_dept_code=dept_code)
  print len(packets)
  print str(type(agent_code)),str(type('Росреестр'))
@@ -56,6 +70,8 @@ def main():
  #p=3
  for pp in range(0,p):
   print packets[pp][0]
+  r=getrecords(cur,packets[pp][0])
+  print "LEN="+str(len(r))
  f.close()
  con.close()
 if __name__ == "__main__":
