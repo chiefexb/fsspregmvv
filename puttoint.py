7423fe406080491e7a5b2ba4ab336c73cd63e653
chiefexb <chief_exb@mail.ru>
2013-12-23 15:31:30 +0400

    Создал по аналогии файл загрузки Пока определяет схему ответа Файл конфигурации единый и на загрузку и на выгрузку

--- puttoint.py
+++ puttoint.py
@@ -1,31 +1,67 @@
 #!/usr/bin/python
 #coding: utf8
-import regmvv
-from config import Config
+from regmvv import *
+from lxml import etree
 from dbfpy import dbf
+import fdb
 import sys
 def main():
+#Обработка параметров
  print len (sys.argv)
  if len(sys.argv)<=1:
   print ("getfromint: нехватает параметров\nИспользование: getfromint ФАЙЛ_КОНФИГУРАЦИИ")
   sys.exit(2)
  print sys.argv[1]
+#Открытие файла конфигурации
  try:
   f=file(sys.argv[1])
  except Exception,e:
   print e
   sys.exit(2)
- cfg = Config(f) 
-#Соединяемся с базой ОСП
- 
+#Парсим xml конфигурации
+ cfg = etree.parse(f) 
+ #cfg.add_namespace(regmvv)
+ cfgroot=cfg.getroot()
+#Ищем параметры системы
+ systemcodepage=cfgroot.find('codepage').text
+#Ищем параметры базы
+ dbparams=cfgroot.find('database_params')
+ username=dbparams.find('username').text
+ password=dbparams.find('password').text
+ hostname=dbparams.find('hostname').text
+ concodepage=dbparams.find('connection_codepage').text
+ codepage=dbparams.find('codepage').text
+ database=dbparams.find('database').text
+ print username,password,hostname,concodepage,codepage
+#Ищем параметры МВВ
+ mvv=cfgroot.find('mvv')
+ agent_code=mvv.find('agent_code').text
+ dept_code=mvv.find('dept_code').text
+ agreement_code=mvv.find('agreement_code').text
+ #f.close()
+#Определяем тип и путь файла
+ filepar=cfgroot.find('file')
+ filecodepage=filepar.find('codepage').text
+ output_path=filepar.find('output_path').text
+ filetype=filepar.find('type').text
+ filenum=filepar.find('numeric').text
+ #Определение схемы файла должна быть ветка для типов файлов пока разбираем xml
+ filiescheme=filepar.findall('scheme')
+ #создание root
+ print filiescheme.getchildren()[0].tag
  try:
-  con = fdb.connect (host=cfg.hostname, database=cfg.database, user=cfg.username, password=cfg.password,charset=cfg.database_connection_codepage)
+  root2=filiescheme[1].getchildren()[0]
+ except:
+  sys.exit(2)
+ 
+#Соединяемся с базой ОСП
+ try:
+  con = fdb.connect (host=hostname, database=database, user=username, password=password,charset=concodepage)
  except  Exception, e:
   print("Ошибка при открытии базы данных:\n"+str(e))
   sys.exit(2)
  cur = con.cursor() 
- db.close() 
- 
+
  f.close()
  con.close()
 if __name__ == "__main__":
