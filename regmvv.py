b02dbd6bbbfb430ed37d77b22c10e9f083bc99ff
chiefexb <chief_exb@mail.ru>
2014-01-11 17:12:45 +0400

    Дата изменения:11.01.2014 Умеет вставлять данные удостоверения личности. Умеет определять тип документа от типа документа росреестра.

--- regmvv.py
+++ regmvv.py
@@ -2,7 +2,7 @@
 #coding: utf8
 const={'er_ext_request_id':0,'er_debtor_inn':1,'er_debtor_kpp':2,'er_req_date':3,'er_pack_date':4,'er_debtor_birthday':5,'er_debtor_ogrn':6,'er_ip_sum':7,'er_processed':8,'er_ip_num':9,'er_req_number':10,'er_mvv_agent_code':11,'er_debtor_document':12,'er_mvv_agreement_code':13,'er_mvv_agent_dept_code':14,'er_pack_number':15,'er_req_id':16,'er_pack_id':17,'er_h_spi':18, 'er_fio_spi':19,'er_osp_number':20,'er_debtor_name':21,'er_debtor_address':22,'er_debtor_birthplace':23,'er_entity_type':24,'er_spi_id':25,'er_ip_id':26,'er_ip_risedate':27,
 'eih_id':0,'eih_pack_number':1,'eih_proceed':2,'eih_agent_code':3,'eih_agent_dept_code':4,'eih_agreement_code':5,'eih_external_key':6,'eih_metaobjectname':7,'eih_date_import':8,'eih_source_barcode':9}
-ansfields ={'01':['ser_doc','num_doc','date_doc','issue_organ'],'11':['kadastr_n','inv_n_nedv','s_nedv','adres_nedv']}
+ansfields ={'01':['ser_doc','num_doc','date_doc','issue_organ','rr_type_doc'],'11':['kadastr_n','inv_n_nedv','s_nedv','adres_nedv']}
 numstr=('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','X','Y','Z')
 cln=', '
 getdivnamesql="select osp.div_fullname_title from osp"
@@ -364,11 +364,12 @@ def setpositive(cur,con,dbsystcp,dbcp,mvv_agent_code,mvv_agreement_code,mvv_dept
  print "LEN ANS:",len(ans),"I AM IN",req_id
  for aa in range(len(ans)):
   print ans[aa][1],aa
+  packid=getgenerator(cur,"DX_PACK")
   if ans[aa][1]=='01':
    id=getgenerator(cur,"SEQ_DOCUMENT")
    ipid=getipid (cur,dbsystcp,dbcp,req_id)
    print "IPID",ipid
-   packid=getgenerator(cur,"DX_PACK")
+  #packid=getgenerator(cur,"DX_PACK")
    hsh=hashlib.md5()
    hsh.update(str(id))
    extkey=hsh.hexdigest()
@@ -395,22 +396,23 @@ def setpositive(cur,con,dbsystcp,dbcp,mvv_agent_code,mvv_agreement_code,mvv_dept
    docs={}
    for dd in ans[aa][2].keys():
     docs[dd]=getxmlvalue(dd,ans[aa],doc)
+   docs=gettypedoc(cur,'UTF-8','CP1251',docs)
+   print docs
    print "Паспорт номер:",docs['ser_doc']," ",docs['num_doc']," ",docs['date_doc'],docs['issue_organ']
-   sq4="INSERT INTO EXT_IDENTIFICATION_DATA (ID, NUM_DOC, DATE_DOC, CODE_DEP, SER_DOC, FIO_DOC, STR_ADDR, ISSUED_DOC) VALUES ("+str(id)+cln+quoted(docs['num_doc'])+cln+quoted(docs['date_doc'])+cln+"NULL"+cln+quoted(docs['ser_doc'])+cln+quoted(ent_name)+cln+"NULL,NULL)"
+   sq4="INSERT INTO EXT_IDENTIFICATION_DATA (ID, NUM_DOC, DATE_DOC, CODE_DEP, SER_DOC, FIO_DOC, STR_ADDR, ISSUED_DOC,type_doc_code) VALUES ("+str(id)+cln+quoted(docs['num_doc'])+cln+quoted(docs['date_doc'])+cln+"NULL"+cln+quoted(docs['ser_doc'])+cln+quoted(ent_name)+cln+"NULL,NULL,"+quoted(docs['type_doc'])+")"
    print "SQ4=",sq4
    cur.execute(sq3.decode('UTF-8').encode('CP1251'))
    con.commit()
    cur.execute(sq4.decode('UTF-8').encode('CP1251'))
    con.commit()
 
-  if ans[aa][1]=='12':
+  if ans[aa][1]=='11':
    rights=a.find(ans[aa][0])
    right=rights.findall(ans[aa][2]['right'])
    for rr in right:
     id=getgenerator(cur,"SEQ_DOCUMENT")
     ipid=getipid (cur,dbsystcp,dbcp,req_id)
     print "IPID",ipid
-    packid=getgenerator(cur,"DX_PACK")
     hsh=hashlib.md5()
     hsh.update(str(id))
     extkey=hsh.hexdigest()
@@ -450,11 +452,11 @@ def setpositive(cur,con,dbsystcp,dbcp,mvv_agent_code,mvv_agreement_code,mvv_dept
     con.commit()
 
  #Заполняем датумы
- #cur.execute(sq)
+#cur.execute(sq)
 #.decode('UTF-8').encode(dbcp))
- #cur.execute(sq2.decode('UTF-8').encode('CP1251'))
+#cur.execute(sq2.decode('UTF-8').encode('CP1251'))
 #.decode('UTF-8').encode(dbcp))
- #con.commit() 
+#con.commit() 
  return
 def getxmlvalue(name,ans,a):
  #Проверка есть ли длинный путь
@@ -504,8 +506,16 @@ def setresponse(cur,con,dbsystcp,dbcp,mvv_agent_code,mvv_agreement_code,mvv_dept
  con.commit()
  cur.execute(sq2.decode('UTF-8').encode('CP1251'))
  con.commit()
-
  return
+def gettypedoc(cur,dbsystcp,dbcp,docs):
+ if 'rr_type_doc' in docs:
+  rr=docs['rr_type_doc']
+  cur.execute('select directory_types.dirt_code from directory_types where directory_types.dirt_code_rosreestr='+quoted(rr))
+  r=cur.fetchall() 
+  print r[0][0]
+  docs['type_doc']=convtotype([' ','C'],r[0][0],'UTF-8','UTF-8')
+ return docs
 #def main():
 #if __name__ == "__main__":
 #    main()
+#type_doc_code
