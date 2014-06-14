0a1aeda5582aef2e6b3e7943671db7aecdecfbe8
chiefexb <chief_exb@mail.ru>
2014-06-14 17:21:38 +0400

    update 14.06.2014 17:17 Добавил схему ответа для банков dbf

--- regmvv.py
+++ regmvv.py
@@ -7,6 +7,7 @@ numstr=('0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H',
 cln=', '
 getdivnamesql="select osp.div_fullname_title from osp"
 getsbnumsql="select counter from sbcount where sbcount.req_date="
+currency_type={'EUR':'978','USD':'840','RUB':'810','GBP':'826'}
 import datetime
 import hashlib
 import logging
